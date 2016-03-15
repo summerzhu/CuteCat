39b6a5d4ec8aaba73f3fcc4b1cca73d6529c9e3f
haifangzhu <5427628072qq.com>
2016-03-15 21:18:28 +0800

    Initial Commit

---
+++ CuteCat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CuteCat
+//
+//  Created by 朱海芳 on 16/3/15.
+//  Copyright © 2016年 朱海芳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CuteCat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CuteCat
+//
+//  Created by 朱海芳 on 16/3/15.
+//  Copyright © 2016年 朱海芳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
